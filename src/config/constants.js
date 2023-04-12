a3c394eaa37de3a143ccae38c9a2019b3987e86b
Ting-Shieh <zsting29@gmail.com>
2023-04-12 20:16:13 +0800

    建築物添加漸變色

---
+++ src/config/constants.js
@@ -0,0 +1,4 @@
+export const color = {
+  mesh: '#1B3045', // 建築物顏色
+  head: '#ffffff' // 建築物頂部顏色
+}
--- src/effect/surroundLine.js
+++ src/effect/surroundLine.js
@@ -1,28 +1,60 @@
 import * as THREE from 'three'
-
+import {color} from '../config/constants.js'
 export class SurroundLine {
   constructor(scene, child) {
     this.scene = scene
     this.child = child
+    // 需要一個模型顏色  最底部顯示的顏色
+    this.meshColor = color.mesh
+    // 需要一個頭部顏色  最頂部顯示顏色
+    this.headColor = color.head
+    // // 高度差 ＝ 最底部 - 最頂部
+    // this.size = 180
+
     this.render()
   }
 
+  computedMesh(){
+    // 動態獲取高度差
+    this.child.geometry.computeBoundingBox()
+    this.child.geometry.computeBoundingSphere()
+  }
   render() {
+    this.computedMesh()
+
+    const {min, max} = this.child.geometry.
+    
+    // 高度差
+    const size = max.z - min.z
+    
     const material = new THREE.ShaderMaterial({ 
       uniforms: {
-        city_color: {
-          value: new THREE.Color('#1B3045')
+        u_city_color: {
+          value: new THREE.Color(this.meshColor)
+        },
+        u_head_color: {
+          value: new THREE.Color(this.headColor)
+        },
+        u_size: {
+          value: size //this.size
         }
       },
       vertexShader: `
+        varying vec3 v_position;
         void main () {
+          v_position = position;
           gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         }
       `,
       fragmentShader: `
-        uniform vec3 city_color;
+        varying vec3 v_position;
+        uniform vec3 u_city_color;
+        uniform vec3 u_head_color;
+        uniform float u_size;
         void main () {
-          gl_FragColor = vec4(city_color, 1.0); // vec4(1.0, 0.0, 0.0, 1.0);
+          vec3 base_color = u_city_color;
+          base_color = mix(base_color, u_head_color, v_position.z / u_size); // 混合好的顏色數據
+          gl_FragColor = vec4(base_color, 1.0); // vec4(1.0, 0.0, 0.0, 1.0);
         }
       `,
      })
